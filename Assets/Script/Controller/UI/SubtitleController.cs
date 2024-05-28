5a6ede1365f2ff71636d06bb653caeda7f57ee4e
rin.lei <1205094889@qq.com>
2024-05-28 13:54:41 +0800

    feat: 字幕自动淡入淡出效果

--- Assets/Script/Controller/UI/SubtitleController.cs
+++ Assets/Script/Controller/UI/SubtitleController.cs
@@ -11,21 +11,69 @@ namespace Script.Controller.UI
     public class SubtitleController : MonoBehaviour
     {
         // textMeshPro
-        public TextMeshProUGUI subtitleText;
         private List<SubtitleEntity> _playingSubtitles = new List<SubtitleEntity>();
+        private TextMeshProUGUI _subtitleText;
+        private CanvasGroup _subtitleCanvasGroup;
 
+        private int _targetVisible = 0;
         private bool _isPlaying = false;
+
+        private bool IsPlaying
+        {
+            get => _isPlaying;
+            set
+            {
+                if (value == _isPlaying) return;
+                // 切换字幕展示
+                ChangeSubtitleVisible(value);
+                _isPlaying = value;
+            }
+        }
+
+        private void Update()
+        {
+            if (_targetVisible < 0)
+            {
+                _subtitleCanvasGroup.alpha -= Time.deltaTime;
+                if (_subtitleCanvasGroup.alpha <= 0)
+                {
+                    _subtitleCanvasGroup.alpha = 0;
+                    _targetVisible = 0;
+                }
+            }
+            else if (_targetVisible > 0)
+            {
+                _subtitleCanvasGroup.alpha += Time.deltaTime;
+                if (_subtitleCanvasGroup.alpha >= 1)
+                {
+                    _subtitleCanvasGroup.alpha = 1;
+                    _targetVisible = 0;
+                }
+            }
+        }
+
+        private void OnEnable()
+        {
+            _subtitleCanvasGroup = GetComponent<CanvasGroup>();
+            _subtitleText = GetComponentInChildren<TextMeshProUGUI>();
+        }
+
+        private void ChangeSubtitleVisible(bool visible)
+        {
+            _targetVisible = visible ? 1 : -1;
+        }
+
 
         // private int _currentSubtitleIndex = -1;
         public bool UpdateSubtitleText(string text)
         {
-            if (subtitleText == null)
+            if (_subtitleText is null)
             {
                 Debug.LogError("Subtitle Text is null");
                 return false;
             }
 
-            subtitleText.text = text;
+            _subtitleText.text = text;
             return true;
         }
 
@@ -34,7 +82,7 @@ namespace Script.Controller.UI
             if (_playingSubtitles.Exists((x) => x.Key == subtitle.Key)) return;
 
             _playingSubtitles.Add(subtitle);
-            if (!_isPlaying) StartPlaySubtitle();
+            if (!IsPlaying) StartPlaySubtitle();
         }
 
         public void StartPlaySubtitle()
@@ -44,7 +92,7 @@ namespace Script.Controller.UI
 
         private IEnumerator PlayCurrentSubtitle()
         {
-            _isPlaying = true;
+            IsPlaying = true;
             var entity = _playingSubtitles.First();
             var duration = entity.Duration;
             var elapsedTime = 0.0f;
@@ -64,13 +112,8 @@ namespace Script.Controller.UI
             else
             {
                 UpdateSubtitleText("");
-                _isPlaying = false;
+                IsPlaying = false;
             }
-        }
-
-
-        private void Update()
-        {
         }
     }
 }
